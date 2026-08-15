5e9c7092b557ff76ececcbe953446f3420eeaddd
Thakwa Hamid Snigdha <thsnigdha33@gmail.com>
2026-08-15 18:54:13 +0600

    Add Hexadecimal to Decimal conversion program

---
+++ HexadecimalToDecimal.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class HexadecimalToDecimal {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Input a hexadecimal number: ");
+        String hexadecimal = input.nextLine();
+
+        int decimal = Integer.parseInt(hexadecimal, 16);
+
+        System.out.println("Equivalent decimal number is: " + decimal);
+    }
+}
