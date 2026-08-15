e268f68fda12589446f2c9e410c7f768ca488e1f
Thakwa Hamid Snigdha <thsnigdha33@gmail.com>
2026-08-15 18:48:33 +0600

    Add DecimalToBinary program to convert numbers

---
+++ DecimalToBinary.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class DecimalToBinary {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Input a Decimal Number: ");
+        int number = input.nextInt();
+
+        String binary = Integer.toBinaryString(number);
+
+        System.out.println("Binary number is: " + binary);
+    }
+}
