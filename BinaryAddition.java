95afc4cfee7bfa6de5f062314e2e64f688e945c9
Thakwa Hamid Snigdha <thsnigdha33@gmail.com>
2026-08-15 18:44:59 +0600

    Update BinaryAddition.java

--- BinaryAddition.java
+++ BinaryAddition.java
@@ -1,1 +1,20 @@
-# OOP-Java-Basic-Programming-Lab-3
+import java.util.Scanner;
+
+public class BinaryAddition {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Input first binary number: ");
+        String binary1 = input.nextLine();
+
+        System.out.print("Input second binary number: ");
+        String binary2 = input.nextLine();
+
+        int num1 = Integer.parseInt(binary1, 2);
+        int num2 = Integer.parseInt(binary2, 2);
+
+        int sum = num1 + num2;
+
+        System.out.println("Sum of two binary numbers: " + Integer.toBinaryString(sum));
+    }
+}
